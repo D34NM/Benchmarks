7929665cc8d0ac7a8a5c5b2ff5e7248ac56c3e54
Dean Milojevic <milojevicdean@gmail.com>
2020-01-26 11:01:28 +0100

    Created initial structure of project

---
+++ src/Benchmark.Linq/Projections/Select.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmark.Linq.Projections
+{
+    public class OtherObject
+    {
+        public string OtherString { get; set; }
+        public long OtherLong { get; set; }
+    }
+
+    public class ComplexObject
+    {
+        public Guid SomeId { get; set; }
+        public string SomeString { get; set; }
+        public int SomeInt { get; set; }
+        public IEnumerable<OtherObject> OtherObjects { get; set; }
+    }
+
+    public class Select
+    {
+        public void Run()
+        {
+
+        }
+
+        [Benchmark]
+        public void ComplexObjects()
+        {
+            
+        }
+    }
+}
